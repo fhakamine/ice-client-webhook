cff5f8d04f1564e753572d2b4486d540ad79d272
Frederico Hakamine <frederico.hakamine@gmail.com>
2017-04-25 11:29:33 -0700

    Cleaning code

--- app.py
+++ app.py
@@ -31,13 +31,19 @@ def webhook():
     #FIGURES OUT WHAT TO DO
     action = req.get("result").get("action")
     if action == "addUser":
-        res = addUser(req)
+        speech = addUser(req)
     elif action == "modUser":
-        res = modUser(req)
+        speech = modUser(req)
     elif action == "resetUser":
-        res = resetUser(req)
+        speech = resetUser(req)
     else:
         return {}
+
+    res = {
+            "speech": speech,
+            "displayText": speech,
+            "source": "apiai-user-webhook-sample"
+          }
 
     res = json.dumps(res, indent=4)
     # print(res)
@@ -45,7 +51,7 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
+#for creating new users
 def addUser(req):
     baseurl = os.environ.get('TENANT_URL')
     key = os.environ.get('API_KEY')
@@ -87,100 +93,22 @@ def addUser(req):
         speech = "User created"
     else:
         speech = "Error "+str(responsecode)
-        #{"errorCode":"E0000001","errorSummary":"Api validation failed: login","errorLink":"E0000001","errorId":"oaeRG1lUiGZTPeCWKfB2s6avw","errorCauses":[{"errorSummary":"login: An object with this field already exists in the current organization"}]}
 
-    # print(json.dumps(item, indent=4))
     print("Response:")
     print(speech)
+    return speech
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-user-webhook-sample"
-    }
-
+#for modifying existing users
 def modUser(req):
-    #define what to do based on the action parameter
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib2.urlencode({'q': yql_query}) + "&format=json"
-    #perform the rest api call
-    result = urllib2.urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
+    speech = "Ops... I don't know how to update a user yet."
+    return speech
 
+#for resetting users
 def resetUser(req):
-    #define what to do based on the action parameter
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib2.urlencode({'q': yql_query}) + "&format=json"
-    #perform the rest api call
-    result = urllib2.urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    #placeholder for variables
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-
+    speech = "Ops... I don't know how to reset a user yet."
+    return speech
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-
     print("Starting app on port %d" % port)
-
     app.run(debug=False, port=port, host='0.0.0.0')
